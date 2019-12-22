0df06affd3d5fc7b1d1e5ffb109cd3d5e1cfbdbe
Jeffrey Wan <jwan@2u.com>
2019-12-22 17:37:35 -0500

    midway through section 10

--- server/index.js
+++ server/index.js
@@ -1,73 +1,70 @@
-const keys = require("./keys");
+const keys = require('./keys');
+
 // Express App Setup
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const app = express(); // recieve and respond to http requests going back and to react server
-app.use(cors()); // allows us to make requests from one domain to another
-app.use(bodyParser.json()); // will parse incoming requests from react to json that express can work with.
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-// Postgres server
+// Postgres Client Setup
 const { Pool } = require('pg');
 const pgClient = new Pool({
-    user: keys.pgUser,
-    host: keys.pgHost,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
 });
-pgClient.on('error', () => console.log("Lost PG Connection"))
+pgClient.on('error', () => console.log('Lost PG connection'));
 
-// store the submitted index value in a table called values with a number column.
 pgClient
-    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
-    .catch((err) => console.log(err));
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+  .catch(err => console.log(err));
 
-
-// Redis setup
-
+// Redis Client Setup
 const redis = require('redis');
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 });
-const redisPublisher = redisClient.duplicate(); // redis documentation because we need individual duplicates to listen, subscribe, and publish. if we use the main one, we cannot use it for the other.
+const redisPublisher = redisClient.duplicate();
 
+// Express route handlers
 
-// express route handlers
 app.get('/', (req, res) => {
-    res.send("hi")
+  res.send('Hi');
 });
 
 app.get('/values/all', async (req, res) => {
-    const values = await pgClient.query("SELECT * from VALUES");
-    // send back to whoever made request
-    res.send(values.rows);
+  const values = await pgClient.query('SELECT * from values');
+
+  res.send(values.rows);
 });
 
 app.get('/values/current', async (req, res) => {
-    redisClient.hgetall('values', (err, values) => {
-        res.send(values)
-    })
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
 });
 
 app.post('/values', async (req, res) => {
-    const index = req.body.index;
-    if (parseInt(index) > 40) {
-       return res.status(422).send("index too high")
-    }
+  const index = req.body.index;
 
-    redisClient.hset('values', index, 'nothing yet!'); // worker will eventually get here and replace it with calculated value
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
 
-    redisPublisher.publish('insert', index); // publishes sends to worker process
-    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-    res.send({ working: true})
+  res.send({ working: true });
 });
 
 app.listen(5000, err => {
-    console.log("listening)")
-})
-
+  console.log('Listening');
+});
--- worker/index.js
+++ worker/index.js
@@ -1,23 +1,19 @@
-// connecting to redis and calculating fib value.
-
-const keys = require('./keys'); // used for connecting to redis
+const keys = require('./keys');
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000 // if connection is lost to redis server, retry once every 1 second or 1000 milis
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 });
-
-const sub = redisClient.duplicate(); // duplicate of client.
+const sub = redisClient.duplicate();
 
 function fib(index) {
-    if (index < 2) return 1;
-    return fib(index-1) + fib(index-2);
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
-sub.on('message', (channel, message) => { // message is the index value of the fibonacci sequence, not the value itself, we have to calc that.
-    redisClient.hset('values', message, fib(parseInt(message))) // this puts it back into redis
-}); // sub on watches redis for new messages
-
-sub.subscribe('insert'); // we subscribe to insert events.
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
+});
+sub.subscribe('insert');
